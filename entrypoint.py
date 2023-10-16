c7b1102dffe02527e1ea698fb4fffc785a75b9e8
miguelmc <mcatedral24@gmail.com>
2023-10-16 02:00:32 -0600

    It change location of file config.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,12 +1,12 @@
 from app import create_app
-from config import config
+from app.config import config
 
 
 configuracion1= config['development']
 configuracion2 = config['production']
 configuracion3 = config['testing']
 
-myApp=create_app(configuracion3)
+myApp=create_app(configuracion1)
 
 if __name__ == '__main__':
     myApp.run()
