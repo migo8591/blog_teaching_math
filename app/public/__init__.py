26c5dabe0b034b5420962bc785dd9edf66084a73
miguelmc <mcatedral24@gmail.com>
2023-10-17 06:31:17 -0600

    Changes are made to the codes styles

--- app/public/__init__.py
+++ app/public/__init__.py
@@ -1,4 +1,4 @@
 from flask import Blueprint
-public_bp = Blueprint('public', __name__, template_folder="templates", static_folder="static")
+public_bp = Blueprint('public', __name__, template_folder="templates", static_folder="public/static")
 
 from . import routes
