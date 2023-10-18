882b003a26d1519660ecb0b0583237b074fb6f88
miguelmc <mcatedral24@gmail.com>
2023-10-18 04:46:43 -0600

    Work is being with Flask migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask 
 from flask_ckeditor import CKEditor
+from flask_migrate import Migrate
 from app.public import public_bp
 from .extensions import db
-
+migrate = Migrate()
 
 def create_app(config):
     app = Flask(__name__)
-    ckeditor = CKEditor(app)
     app.config.from_object(config)
+    migrate = Migrate(app, db)
+    ckeditor = CKEditor(app)
     app.register_blueprint(public_bp)
     db.init_app(app)
+    migrate.init_app(app, db)
     with app.app_context():
         db.create_all()
     return app
--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from .extensions import db
+from flask_login import UserMixin
 from datetime import datetime
 
 class Posts(db.Model):
@@ -8,3 +9,22 @@ class Posts(db.Model):
     date_posted = db.Column(db.DateTime, default= datetime.utcnow)
     slug = db.Column(db.String(255))
     # poster_id= db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class Users(db.Model, UserMixin):
+    id=db.Column(db.Integer, primary_key=True )
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    name=db.Column(db.String(200), nullable=False)
+    email=db.Column(db.String(120), nullable=False, unique=True)
+    about_me=db.Column(db.Text(500), nullable=True)
+    # profile_pic=db.Column(db.String(120), nullable=True)
+    date_added=db.Column(db.DateTime, default=datetime.utcnow)
+    password_hash = db.Column(db.String(150))
+    posts = db.relationship('Posts', backref='poster')
+
+    # The serialize method converts the object to a dictionary
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "emai": self.email
+        }
