193c1a8f0f340ccdecbf3a042686c33009f05dd3
miguelmc <mcatedral24@gmail.com>
2023-10-23 00:15:45 -0600

    Deleting comment of python second time

--- app/public/routes.py
+++ app/public/routes.py
@@ -44,28 +44,17 @@ def editPost(id):
         db.session.commit()
         flash("Post ha sido actualizado")
         return redirect(url_for('public.post', id=id))
-    # if current_user.id == post.poster_id:
     else:
         form.title.data=post.title
-        # form.author.data=post.author
         form.slug.data=post.slug
         form.content.data=post.content
         return render_template('public/edit.html', form=form)
-        # return redirect(url_for('public.post', id=id, form=form))
-        # return render_template('public/editPost.html', form=form)
-    # else:
-    #     flash("You Aren't Authorized to Edit This Post...")
-    #     return render_template('public/editPost.html', form=form)
 @public_bp.route("/deletePost<int:id>")
 def deletePost(id):
      postDelete=Posts.query.get_or_404(id)
      db.session.delete(postDelete)
      db.session.commit()
      flash("¡Post fue eliminado...!")
-# @public_bp.context_processor
-# def base():
-#     form = SearchForm()
-#     return dict(form=form)
 @public_bp.route('/search', methods=["POST"])
 def search():
     formulario = SearchForm()
@@ -75,21 +64,3 @@ def search():
         posts = posts.filter(Posts.content.like('%'+post.searched+'%'))
         posts = posts.order_by(Posts.title).all()
         return render_template("public/search.html", form=formulario, searched=post.searched, posts=posts )
-# def deletePost(id):
-#     postDelete=Posts.query.get_or_404(id)
-#     ide=current_user.id
-#     if ide== postDelete.poster.id:       
-#         try:
-#             db.session.delete(postDelete)
-#             db.session.commit()
-#             flash("¡Post fue eliminado...!")
-#             return redirect(url_for('public.index'))
-        
-#         except:
-#                 flash("Whoops! There was a problem deleting post")      
-#                 #Grab all the posts from the databases:
-#                 posts = Posts.query.order_by(Posts.date_posted)
-#                 return render_template("public/posts.html", posts= posts)
-#     else:
-#         flash("You Aren't Authorized to Delete This Post")
-#         return redirect(url_for('public.post', id=id))
